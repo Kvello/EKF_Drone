8d9b12ae9deef6c9a3dbf3519eb45345da431ff5
kvello <markus.kv1@gmail.com>
2024-03-25 13:23:21 +0800

    Corrects path for sjtu_drone package

--- bd.sh
+++ bd.sh
@@ -1,4 +1,4 @@
 . params.sh
 
-colcon build --symlink-install #--cmake-args -DCMAKE_BUILD_TYPE=Release #--packages-select my_package
+colcon build --packages-skip-regex sjtu_drone* --symlink-install #--cmake-args -DCMAKE_BUILD_TYPE=Release #--packages-select my_package
 . install/setup.bash
